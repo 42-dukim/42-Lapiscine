3df552210783a017e8fc1c173c65c1e2ee0188f8
Duhyun Kim <dukim@c1r5s6.42gyeongsan.kr>
2024-01-13 22:21:55 +0900

    fix: while 문으로 변경

--- c00/ex01/ft_print_alphabet.c
+++ c00/ex01/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 21:53:05 by dukim             #+#    #+#             */
-/*   Updated: 2024/01/13 22:06:36 by dukim            ###   ########.fr       */
+/*   Updated: 2024/01/13 22:21:04 by dukim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 void	ft_print_alphabet(void)
 {
-	char	alphabet[27];
+	char	a;
 
-	alphabet = "abcdefghijklmnopqrstuvwxyz";
-	write(1, alphabet, 26);
+	a = 'a';
+	while (a != 'z')
+	{
+		write(1, &a, 1);
+		a++;
+	}
 }
