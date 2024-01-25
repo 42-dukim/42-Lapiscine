46b8801c17c54f57ea969180e7e24cff006ef4e8
Duhyun Kim <dukim@c1r4s7.42gyeongsan.kr>
2024-01-25 21:12:14 +0900

    update: && -> || 으로 변경해 추후 비교문 삭제

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: dukim <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 19:36:13 by dukim             #+#    #+#             */
-/*   Updated: 2024/01/19 19:50:09 by dukim            ###   ########.fr       */
+/*   Updated: 2024/01/25 20:10:45 by dukim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
 			return (1);
@@ -23,10 +23,24 @@ int	ft_strcmp(char *s1, char *s2)
 			return (-1);
 		i++;
 	}
-	if (s1[i] > s2[i])
-		return (1);
-	else if (s1[i] < s2[i])
-		return (-1);
-	else
-		return (0);
+	return (0);
 }
+/*
+#include <stdio.h>
+
+int	main()
+{
+	char *str1 = "1234";
+	char *str2 = "12345";
+	char *str3 = "abcdefg";
+	char *str4 = "ABCDEFG";
+	char *str5 = "";
+	char *str6 = "h";
+	char *str7 = "!@#$%";
+	char *str8 = "!@#$%";
+
+	printf("%s <-> %s: %d\n", str1, str2, ft_strcmp(str1, str2)); 
+	printf("%s <-> %s: %d\n", str3, str4, ft_strcmp(str3, str4)); 
+	printf("%s <-> %s: %d\n", str5, str6, ft_strcmp(str5, str6)); 
+	printf("%s <-> %s: %d\n", str7, str8, ft_strcmp(str7, str8)); 
+}*/
