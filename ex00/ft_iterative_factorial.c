e5494a67b768880fbe1d9291287564a15173d225
Duhyun Kim <dukim@c1r4s7.42gyeongsan.kr>
2024-01-26 01:31:34 +0900

    added ex00

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 01:27:19 by dukim             #+#    #+#             */
+/*   Updated: 2024/01/26 01:30:59 by dukim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	i;
+	int	factorial;
+
+	if (nb <= 0)
+		return (0);
+	i = 1;
+	factorial = 1;
+	while (i <= nb)
+		factorial *= i++;
+	return (factorial);
+}
+
+#include <stdio.h>
+
+int	main()
+{
+	printf("%d\n", ft_iterative_factorial(10));
+}
