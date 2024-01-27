3059927144745ceb00ce2d1fc80573667ba22730
Duhyun Kim <dukim@c1r4s5.42gyeongsan.kr>
2024-01-27 16:25:05 +0900

    fix: is_in_str method에 str_slice 널문자 조건 추가

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: dukim <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 18:11:29 by dukim             #+#    #+#             */
-/*   Updated: 2024/01/25 21:01:17 by dukim            ###   ########.fr       */
+/*   Updated: 2024/01/27 16:22:44 by dukim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	is_in_str(char *str_slice, char *to_find)
 	int	i;
 
 	i = 0;
-	while (to_find[i] != '\0')
+	while (str_slice[i] != '\0' && to_find[i] != '\0')
 	{
 		if (str_slice[i] != to_find[i])
 			return (0);
@@ -38,12 +38,12 @@ char	*ft_strstr(char *str, char *to_find)
 	return (0);
 }
 /*
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
 int	main()
 {
-	char *to_find = "";
+	char *to_find = "1";
 	char *str = "123 456";
 	char *ptr = ft_strstr(str, to_find);
 
@@ -51,7 +51,7 @@ int	main()
 		printf("%s\n", ptr);
 	else
 		printf("is Null!\n");
-	
+
 	ptr = strstr(str, to_find);
 	if (ptr != NULL)
 		printf("%s\n", ptr);
