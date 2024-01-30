4fe76334c9066593aeebe4ca4865010c4bfa32ef
Duhyun Kim <dukim@c1r4s4.42gyeongsan.kr>
2024-01-30 21:54:59 +0900

    added ex00

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/28 00:24:21 by dukim             #+#    #+#             */
+/*   Updated: 2024/01/28 03:09:01 by dukim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*dup;
+	int		len;
+	int		i;
+
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	dup = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+/*
+#include <stdio.h>
+
+int	main()
+{
+	char *dup;
+
+	dup = ft_strdup("sadlkfjasdlfjsdlfjlsk;dfjl;xvxkvl;jx;lv");
+	printf("%s\n", dup);
+}*/
