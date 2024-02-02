12037ce985d7e4770924420b0142c757abba585a
Duhyun Kim <dukim@c1r4s7.42gyeongsan.kr>
2024-02-02 20:49:09 +0900

    fix: ) 추가

--- ex02/ft_abs.h
+++ ex02/ft_abs.h
@@ -6,13 +6,13 @@
 /*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 17:25:17 by dukim             #+#    #+#             */
-/*   Updated: 2024/02/02 20:34:45 by dukim            ###   ########.fr       */
+/*   Updated: 2024/02/02 20:48:49 by dukim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-# define ABS(Value) (Value * ((Value > 0) - (Value < 0))
+# define ABS(Value) (Value * ((Value > 0) - (Value < 0)))
 
 #endif
