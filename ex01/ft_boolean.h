067cf1dee5cbfb333c781f28b821da87e65bae72
Duhyun Kim <dukim@c1r4s7.42gyeongsan.kr>
2024-02-02 20:47:50 +0900

    fix: 삼항 연산자 삭제 후 다른 로직 사용

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 04:11:59 by dukim             #+#    #+#             */
-/*   Updated: 2024/02/02 17:23:50 by dukim            ###   ########.fr       */
+/*   Updated: 2024/02/02 20:33:52 by dukim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 # define TRUE 1
 # define FALSE 0
-# define EVEN(nbr) ((nbr % 2 == 0) ? 1 : 0)
+# define EVEN(nbr) (nbr % 2 == 0)
 # define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
