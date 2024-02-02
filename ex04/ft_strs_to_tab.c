b0d64283ca3ed3dfe08b5fa896887f8573d9b508
Duhyun Kim <dukim@c1r4s7.42gyeongsan.kr>
2024-02-02 20:32:26 +0900

    fix: -> 연산자에서 dot(.) 연산자로 변경

--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -6,16 +6,16 @@
 /*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 17:38:04 by dukim             #+#    #+#             */
-/*   Updated: 2024/02/02 18:53:59 by dukim            ###   ########.fr       */
+/*   Updated: 2024/02/02 20:30:17 by dukim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "ft_stock_str.h"
 
 void	str_cpy(char *dest, char *src)
 {
 	int	i;
-	int	src_len;
 
 	i = 0;
 	while (src[i] != '\0')
@@ -38,23 +38,23 @@ int	str_len(char *str)
 
 struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
-	struct s_stock_str	stock;
-	int					i;
+	t_stock_str	*stock;
+	int			i;
 
-	stock = (struct s_stock_str *)(sizeof(struct s_stock_str) * (ac + 1));
+	stock = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
 	if (!stock)
 		return (0);
 	i = 0;
 	while (i < ac)
 	{
-		size = str_len(av[i]);
-		stock[i]->str = av[i];
-		stock[i]->copy = (char *)malloc(sizeof(char *) * (size + 1));
-		str_cpy(stroc[i]->copy, av[i]);
+		stock[i].size = str_len(av[i]);
+		stock[i].str = av[i];
+		stock[i].copy = (char *)malloc(sizeof(char *) * (stock[i].size + 1));
+		str_cpy(stock[i].copy, av[i]);
 		i++;
 	}
-	stock[i]->size = 0;
-	stock[i]->str = 0;
-	stock[i]->copy = 0;
+	stock[i].size = 0;
+	stock[i].str = 0;
+	stock[i].copy = 0;
 	return (stock);
 }
