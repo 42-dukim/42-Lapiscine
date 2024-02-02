4fc66bd40bcf96382d568eb38e26f9da1c46f8f5
Duhyun Kim <dukim@c1r4s7.42gyeongsan.kr>
2024-02-02 17:24:50 +0900

    added ex00

---
+++ ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 04:08:38 by dukim             #+#    #+#             */
+/*   Updated: 2024/02/02 17:13:36 by dukim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
